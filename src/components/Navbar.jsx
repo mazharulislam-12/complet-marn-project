ffb402bfab36aa93fc0d2c5ddab23913c2aa9f18
Mazharul Islam <505mazharulislam@gmail.com>
2024-02-10 17:05:26 +0600

    navbar crate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,76 @@
 import React from 'react';
+import logo from '/logo.png';
+import { BiSolidPhoneCall } from "react-icons/bi";
 
 const Navbar = () => {
+
+    const navItem = <>
+        <li><a href='/'>Home</a></li>
+        <li>
+            <details>
+                <summary>Menu</summary>
+                <ul className="p-2">
+                    <li><a>All</a></li>
+                    <li><a>Salad</a></li>
+                    <li><a>Pizza</a></li>
+                </ul>
+            </details>
+        </li>
+        <li>
+            <details>
+                <summary>SErvice</summary>
+                <ul className="p-2">
+                    <li><a>Online Order</a></li>
+                    <li><a>Table Booking</a></li>
+                    <li><a>Order Tracking</a></li>
+                </ul>
+            </details>
+        </li>
+        <li><a>Offers</a></li>
+    </>
+
     return (
-        <div>
-            Navbar
-        </div>
+        <header className='max-w-7xl mx-auto'>
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            {navItem}
+                        </ul>
+                    </div>
+                    <a href='/'>
+                        <img src={logo} alt="" />
+                    </a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {navItem}
+                    </ul>
+                </div>
+                <div className="navbar-end flex items-center ">
+
+                    {/* such btn */}
+                    <button className="btn btn-ghost btn-circle mr-7 hidden lg:flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                    </button>
+
+                    {/* cart item */}
+                    <div className="indicator mr-7 cursor-pointer hidden lg:flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                        <span className="badge badge-sm indicator-item">8</span>
+                    </div>
+
+                    {/* btn */}
+                    <a className="btn bg-green text-white rounded-full px-6 flex items-center gap-2">
+                        <BiSolidPhoneCall />
+                        Contact
+                    </a>
+                </div>
+            </div>
+        </header>
     );
 };
 
